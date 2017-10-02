55385d92d36f5ec000819958a6e2ecb529db2fb5
Hannah Danziger <hdanziger8@gmail.com>
2017-10-02 20:03:05 +0000

    automatically backed up by learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,4 +1,5 @@
-module Dance
+module FancyDance
+  module InstanceMethods
 
   def twirl
     "I'm twirling!"
@@ -14,5 +15,14 @@ module Dance
 
   def take_a_bow
     "Thank you, thank you. It was a pleasure to dance for you all."
+  end
+end
+
+
+  module ClassMethods
+
+    def metadata
+      "This class produces objects that love to dance."
+    end
   end
 end
